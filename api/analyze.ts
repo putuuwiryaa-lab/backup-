b64b24068e374649ab50ebdfa61fe0c0a1619a5b
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-04-23 14:15:46 +0800

    Update analyze.ts

--- api/analyze.ts
+++ api/analyze.ts
@@ -1,4 +1,4 @@
-import { runAnalysis } from "./predictionEngine";
+import { runAnalysis } from "./predictionEngine.js";
 
 export default async function handler(req: any, res: any) {
   if (req.method !== "POST") {
