9b6110b1bdfca75deb2896f2e492388373aeffa1
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-04-30 14:38:29 +0800

    Update admin.ts

--- api/admin.ts
+++ api/admin.ts
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export default async function handler(req: any, res: any) {
