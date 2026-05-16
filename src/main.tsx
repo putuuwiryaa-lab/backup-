a9af2a70a0b702589d46b80f48bc85e339f13399
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-05-16 22:30:29 +0800

    Load fit-to-card chip override

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './responsive-results.css';
+import './fit-result-chips.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
