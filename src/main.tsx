4f85b09e105c413d8426275fe8de68d253f21a70
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-05-22 22:56:46 +0800

    Remove pasted PWA icon CSS import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './ui/design-system.css';
 import './ui/theme-polish.css';
-import './ui/pwa-icon-header.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
