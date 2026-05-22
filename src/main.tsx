7ef3a7f156d7cc14604fb44ad92f0df4a81fded3
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-05-22 18:44:26 +0800

    Load global theme polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './ui/design-system.css';
+import './ui/theme-polish.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
