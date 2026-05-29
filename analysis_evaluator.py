d1dbfc421fabe711ae0cc78c2e3600f287484928
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-05-29 21:47:01 +0800

    Refresh rekap watchlist after analysis evaluation

--- analysis_evaluator.py
+++ analysis_evaluator.py
@@ -1,5 +1,7 @@
 from evaluator.processor import run_all_markets
+from evaluator.rekap_watch import refresh_rekap_watchlist
 
 
 if __name__ == "__main__":
     run_all_markets()
+    refresh_rekap_watchlist()
