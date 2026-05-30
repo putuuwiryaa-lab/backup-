bd859f13cd367cf365709d470f29caf0f91642fd
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-05-30 14:52:35 +0800

    Remove old rekap watch refresh

--- analysis_evaluator.py
+++ analysis_evaluator.py
@@ -1,9 +1,7 @@
 from evaluator.processor import run_all_markets
-from evaluator.rekap_watch import refresh_rekap_watchlist
 from evaluator.market_statistics import refresh_market_statistics
 
 
 if __name__ == "__main__":
     run_all_markets()
-    refresh_rekap_watchlist()
     refresh_market_statistics()
