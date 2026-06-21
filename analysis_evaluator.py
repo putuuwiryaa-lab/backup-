cc0a3fac66042fa74aaf8be896d93931868d514d
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-06-21 17:16:05 +0800

    Fix analysis evaluator statistics import

--- analysis_evaluator.py
+++ analysis_evaluator.py
@@ -1,7 +1,7 @@
 from evaluator.processor import run_all_markets
-from evaluator.market_statistics import refresh_market_statistics
+from evaluator.market_statistics import rebuild_market_statistics
 
 
 if __name__ == "__main__":
     run_all_markets()
-    refresh_market_statistics()
+    rebuild_market_statistics()
