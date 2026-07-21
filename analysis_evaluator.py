3f7468de4f8197a5b932d0d7ce3120a75911a70c
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-07-21 23:38:18 +0800

    refactor: separate analysis evaluator from statistics rebuild

--- analysis_evaluator.py
+++ analysis_evaluator.py
@@ -1,7 +1,5 @@
 from evaluator.processor import run_all_markets
-from evaluator.market_statistics import rebuild_market_statistics
 
 
 if __name__ == "__main__":
     run_all_markets()
-    rebuild_market_statistics()
